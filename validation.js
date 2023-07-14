8ceff525b61de210d3c108b70f54d9326cb9dac0
ninotermulo <ninotermulo01@gmail.com>
2023-07-14 20:42:33 +0800

    Add validation script to js file

--- validation.js
+++ validation.js
@@ -1,1 +1,13 @@
-// validation script here
+const inputs = document.querySelectorAll('input');
+// Loop through each collection of inputs
+// Add Event Listener for each 
+
+const patterns = {
+    telephone: /^\d{11}$/
+};
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        console.log(e.target.attribute.name.value);
+    });
+});
